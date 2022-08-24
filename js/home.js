ac8ce98d71b29e7a6e3af1cd22b423dda2b16915
Sam Rankin <srankin@riester.com>
2022-08-23 20:33:50 -0700

    Adding home page script

---
+++ js/home.js
@@ -0,0 +1,11 @@
+/*
+ *
+ * home
+ * @author    Sam Rankin <srankin@riester.com>
+ * @copyright 2022 RIESTER
+ * @version   <<projectversion>>
+ */
+
+let jobDescriptions = document.querySelectorAll('#block-jobs .notion-collection-list__item-content .notion-property__text');
+
+console.log(jobDescriptions);
